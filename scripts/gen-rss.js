832ed5cbf0820e81341e4fecbe7a6f7c4c67fd80
purva <purvagevariya@gmail.com>
2024-08-01 19:21:02 -0400

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,7 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Purva Gevaria'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
